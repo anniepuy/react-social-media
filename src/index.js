14286b94f8c279c6bb4462b3ed1a531c6133c304
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-02-11 12:43:45 -0600

    created component structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const baseURL = "https//api.github.com/users/";
 
-function App() {
-    return (
-        <div>
-            app;
-        </div>
-    )
-}
 
 const rootNode = document.getElementsByID("root");
 ReactDOM.render(<App />, rootNode);
