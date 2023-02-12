b84dff11e88b711febe36a89c7eca0d587e4532d
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-02-12 10:10:15 -0600

    added reducer to delete a post

--- src/reducer.js
+++ src/reducer.js
@@ -5,9 +5,9 @@ function postReducer(state, action) {
             return {posts: [newPost, ...state.posts]};
         }
         case "DELETE_POST": {
-
+            const deletedPostId = action.payload.id;
+            return {posts: state.posts.filter(post => post.id !== deletedPostId)}
         }
-        break
         default:
             return state;
     }
