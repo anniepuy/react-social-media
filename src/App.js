5acab579c506c875e9c5c5cfa2aa3b720b5e3a79
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-02-11 21:05:15 -0600

    adding state to the posts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Login from "./components/Login";
 import Header from "./components/Header";
+import CreatePost from "./components/CreatePost";
 
 function App() {
     const [user, setUser] = React.useState("ann");
+    const [posts, setPosts] = React.useState([]);
 
     React.useEffect(() => {
         document.title = user ? `${user}'s Feed` : "Please Login";
@@ -16,8 +18,22 @@ function App() {
     return (
         <div>
             <Header user={user} setUser={setUser}/>
+            <CreatePost user={user} setPosts={setPosts} posts={posts}/>
+            {posts.map((post, i) => (
+                <React.Fragment key={i}>
+                    {post.image && (
+                       <img
+                            style={{height: 100, width: 200, objectFit: "cover"}}
+                            src ={URL.createObjectURL(post.image)}
+                            alt="Post Cover"
+                        />
+                    )}
+                    <p>{post.content}</p>
+                    <div>{user}</div> 
+                </React.Fragment>
+            ))}
         </div>
-    )
+    );
 }
 
 export default App;
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,10 +1,35 @@
 import React from "react";
 
 
-function CreatePost() {
+function CreatePost({user, setPosts, posts}) {
+    const [content, setContent] = React.useState('');
+    const [image, setImage] = React.useState(null);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        //const post = { content: content, image: image, user: user};
+        //shortened version of above
+        const post = {content, image, user};
+        const newPosts = [post, ...posts];
+        setPosts(newPosts);
+    }
+
     return (
         <div>
-            create post
+            <h1>Create New Post</h1>
+            <form onSubmit={handleSubmit}>
+                <input  
+                    type="text"
+                    placeholder="Add Post Comment"
+                    onChange = {event => setContent(event.target.value)}
+                />
+                <input
+                    type="file"
+                    onChange = {event => setImage(event.target.files[0])}
+                />
+                <button type="submit">Submit</button>
+            </form>
+            
         </div>
     )
 }
