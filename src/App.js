1995228d89d59c904e04b6c4ced4825abd84f0d7
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-02-12 09:28:54 -0600

    added Context to pass user

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import CreatePost from "./components/CreatePost";
 import PostList from "./components/PostList";
 
+export const UserContext = React.createContext();
 
 function App() {
     const [user, setUser] = React.useState("ann");
@@ -26,11 +27,11 @@ function App() {
     }
 
     return (
-        <div>
+        <UserContext.Provider value={user}>
             <Header user={user} setUser={setUser}/>
             <CreatePost user={user} handleAddPost ={handleAddPost}/>
             <PostList posts={posts}/>
-        </div>
+        </UserContext.Provider>
     );
 }
 
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { UserContext } from "../App";
 
 function Post({image, content, user}) {
+    const currentUser = React.useContext(UserContext);
+    const isCurrentUser = currentUser === user;
+
     return (
         <>
             {image && (
                 <img
-                    style={{height: 100, width: 200, objectFit: "cover"}}
-                    src ={URL.createObjectURL(image)}
-                    alt="Post Cover"
+                style={{height: 100, width: 200, objectFit: "cover"}}
+                src ={URL.createObjectURL(image)}
+                alt="Post Cover"
                 />
             )}
             <p>{content}</p>
-            <div>{user}</div> 
+            <div style={{color: isCurrentUser && 'green'}}>{user}</div> 
         </>
-
     )
 }
 
